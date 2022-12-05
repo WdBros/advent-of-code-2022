6dc43660d033992b63988b7afc7404d3b8e02626
WdBros <wdbros@gmail.com>
2022-12-05 13:38:11 +0300

    add Day3

---
+++ Day3.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+// Part 1 of Day 3
+class Day3
+{
+    static void Main(string[] args)
+    {
+        string letters = "abcdefghijklmnopqrstuvwxyz"; letters += letters.ToUpper(); letters = "0" + letters;
+
+        string[] input = File.ReadAllLines("input.txt");
+
+        int runningTotal = 0;
+
+        foreach (string line in input)
+        {
+            string firstHalf = line.Substring(0, line.Length / 2);
+            string secondHalf = line.Substring(line.Length / 2, line.Length / 2);
+
+            foreach (char item in firstHalf)
+            {
+                if (secondHalf.Contains(item))
+                {
+                    runningTotal += letters.IndexOf(item);
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine("The total is: " + runningTotal);
+
+        Console.ReadKey();
+    }
+}
+
